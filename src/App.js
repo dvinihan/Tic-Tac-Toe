ad8869e88f21b2f5fc5a09c5301a1743f56ce6d7
dvinitsky <daniel.vinitsky@gmail.com>
2018-04-22 23:25:44 -0500

    Added clicking

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class TicTacToeBoard extends Component {
+		
+  render() {
+    return (
+      <div className = "board">
+        <div onClick={this.props.onclick} className ="one spot"></div>
+        <div onClick={this.props.onclick} className ="two spot"></div>
+        <div onClick={this.props.onclick} className ="three spot"></div>
+        <div onClick={this.props.onclick} className ="four spot"></div>
+        <div onClick={this.props.onclick} className ="five spot"></div>
+        <div onClick={this.props.onclick} className ="six spot"></div>
+        <div onClick={this.props.onclick} className ="seven spot"></div>
+        <div onClick={this.props.onclick} className ="eight spot"></div>
+        <div onClick={this.props.onclick} className ="nine spot"></div>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+		this.updateDOM = this.updateDOM.bind(this);
+		this.state = {
+			isXsTurn: true
+		}
+	}
+	
+	updateDOM(spot, team){
+			spot.innerHTML = team;		
+			this.setState({isXsTurn: team === "O" ? true : false});
+			document.getElementsByClassName("turn")[0].innerHTML = `It's ${this.state.isXsTurn ? "O" : "X"}'s turn`;
+	}
+	
+	handleClick(e) {
+		if(e.target.innerHTML !== ""){
+			return;
+		}
+			 
+		if(this.state.isXsTurn){
+			this.updateDOM(e.target, "X");
+		} 
+		else {
+			this.updateDOM(e.target, "O");
+		}
+		
+	}
+	
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="header">Tic Tac Toe</h1>
+				<div className = "turn">It's X's turn</div>
+
+        <TicTacToeBoard  onclick={this.handleClick} />
+
+
       </div>
     );
   }
